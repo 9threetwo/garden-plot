c1f13e0ffc7374988bbbb382acf3e0cc4968e5b2
Sofya Govorova <91029989+GovorovaSofya@users.noreply.github.com>
2021-12-07 11:11:28 +0300

    all files

---
+++ js/vegetables.js
@@ -0,0 +1,52 @@
+let character_array = [
+    {
+        name: "og",
+        width: "241.5px",
+        char_text: "<b>Огурец</b> "
+    },
+    {
+        name: "tom",
+        width: "345.83px",
+        char_text: "<b>Томат</b> "
+    },
+    {
+        name: "mor",
+        width: "453.167px",
+        char_text: "<b>Морковь</b> "
+    },
+    {
+        name: "sv",
+        width: "371.5px",
+        char_text: "<b>Свекла</b> "
+    },
+    {
+        name: "potato",
+        width: "373.83px",
+        char_text: "<b>Картошка</b> "
+    },
+    {
+        name: "kab",
+        width: "371.83px",
+        char_text: "<b>Кабачок</b> "
+    },
+    {
+        name: "kap",
+        width: "487.9px",
+        char_text: "<b>Капуста</b> "
+    },
+    {
+        name: "tik",
+        width: "513.167px",
+        char_text: "<b>Тыква</b> "
+    },
+    {
+        name: "kyk",
+        width: "487.9px",
+        char_text: "<b>Кукуруза</b> "
+    },
+    {
+        name: "paper",
+        width: "513.167px",
+        char_text: "<b>Болгарский перец</b> "
+    }
+]
